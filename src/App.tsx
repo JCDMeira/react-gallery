fc81113a4b0eb7870fe4ff05f2d0cdcfc2c8ffc3
Jean Meira <jean.meira10@hotmail.com>
2023-09-15 20:53:51 -0300

    feat: add core app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,99 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { SyntheticEvent, useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./App.css";
+import { Photo } from "./components";
+
+const clientID = `?client_id=${import.meta.env.VITE_API_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
 function App() {
+  const [loading, setLoading] = useState(false); //loading
+  const [photos, setPhotos] = useState<any[]>([]); //photos
+  const [page, setPage] = useState(1); //page
+  const [query, setQuery] = useState(""); ///query
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos((oldPhoto) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhoto, ...data.results];
+        } else {
+          return [...oldPhoto, ...data];
+        }
+      });
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", () => {});
+  }, []);
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setPage(1);
+    fetchImages();
+  };
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
