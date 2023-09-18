242afbf54558b1693187cf93de9f96882b0a3451
Jean Meira <jean.meira10@hotmail.com>
2023-09-18 19:39:22 -0300

    feat: add store

---
+++ src/store.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { create } from "zustand";
+
+interface IImageStore {
+  photos: any[];
+  setPhotos: (photos: any[]) => void;
+  query: string;
+  setQuery: (query: string) => void;
+  page: number;
+  setPage: (page: number) => void;
+}
+
+export const imagesStore = create<IImageStore>((set) => ({
+  photos: [],
+  setPhotos: (photos) =>
+    set((state) => {
+      if (state.query && state.page === 1) return { ...state, photos };
+      return { ...state, photos: [...state.photos, ...photos] };
+    }),
+  query: "",
+  setQuery: (query) => set((state) => ({ ...state, query })),
+  page: 1,
+  setPage: (page) => set((state) => ({ ...state, page })),
+}));
