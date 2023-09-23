03720197f5c74133a4aafa70e66221ec6eb6dbee
Jean Meira <jean.meira10@hotmail.com>
2023-09-23 13:53:37 -0300

    feat: add querys on url

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,6 @@ import { getImages } from "./service";
 
 interface IImageStore {
   photos: any[];
-  query: string;
   page: number;
   isLoading: boolean;
   setQuery: (query: string) => void;
@@ -15,14 +14,28 @@ interface IImageStore {
   finish_FetchData: () => void;
 }
 
+const setQuey = (key: string, value: string) => {
+  if ("URLSearchParams" in window) {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+    const newRelativePathQuery =
+      window.location.pathname + "?" + searchParams.toString();
+    history.pushState(null, "", newRelativePathQuery);
+  }
+};
+const getQuey = (key: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  return searchParams.get(key) || "";
+};
+
 export const imagesStore = create<IImageStore>((set, get) => ({
   photos: [],
-  query: "",
   page: 1,
   isLoading: false,
   setQuery: (query) => {
     const reset_Fetch = get().reset_Fetch;
     set((state) => ({ ...state, query }));
+    setQuey("query", query);
     reset_Fetch();
   },
   reset_Fetch: () => {
@@ -32,14 +45,14 @@ export const imagesStore = create<IImageStore>((set, get) => ({
   },
   fetchData: async () => {
     const page = get().page;
-    const query = get().query;
+    const query = getQuey("query");
     const init_FetchData = get().init_FetchData;
     const finish_FetchData = get().finish_FetchData;
     init_FetchData();
     const data = await getImages({ page, query });
     finish_FetchData();
     set((state) => {
-      if (state.query && state.page === 1) {
+      if (query && state.page === 1) {
         return { ...state, photos: data };
       } else if (state.page === 1) {
         return { ...state, photos: data };
